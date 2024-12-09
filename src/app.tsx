b135ffe7cfc8c1129c3bec2ceec245c29e3a24d1
Muhmmadamin <uniteenot102@gmail.com>
2024-12-09 13:51:19 +0500

    Work-Out

---
+++ src/app.tsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Navbar from "@/components/shared/navbar";
+import Auth from "./pages/auth";
+import NoteFound from "./pages/note-found";
+import Dashboard from "./pages/dashboard";
+import { Toaster } from "sonner";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/">
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="*" element={<NoteFound />} />
+      </Routes>
+      <Toaster position="top-center" />
+    </>
+  );
+};
+
+export default App;
---
+++ src/firebase/fire-config.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBm-jq_RhvTMj-H5Kl7qEvgv8skwwckMKE",
+  authDomain: "gym-pro-ae5b5.firebaseapp.com",
+  projectId: "gym-pro-ae5b5",
+  storageBucket: "gym-pro-ae5b5.appspot.com",
+  messagingSenderId: "601207742172",
+  appId: "1:601207742172:web:a3c05b4dbfcacb083a1e19",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+export { auth, db };
